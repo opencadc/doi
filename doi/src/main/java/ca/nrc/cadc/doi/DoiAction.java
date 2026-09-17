0c82072f26dcf761f57e5e6b7d21248a37a0af44
Jeff Burke <jburke.cadc@gmail.com>
2026-09-17 13:38:21 -0700

    catch runtime when unable to get the recursive lock nodes job phase

--- doi/src/main/java/ca/nrc/cadc/doi/DoiAction.java
+++ doi/src/main/java/ca/nrc/cadc/doi/DoiAction.java
@@ -418,44 +418,54 @@ public abstract class DoiAction extends RestAction {
                 VOSURI vosuri = new VOSURI(vaultResourceID, String.format("%s/%s", parentPath, doiContainerNode.getName()));
                 RecursiveSetNode recursiveSetNode = new RecursiveSetNode(jobURL, doiContainerNode);
                 recursiveSetNode.setSchemaValidation(false);
-                ExecutionPhase phase = recursiveSetNode.getPhase(20); // seconds
-                switch (phase) {
-                    case COMPLETED:
-                    case ARCHIVED:
-                        // job finished, set corresponding status
-                        if (status.equals(Status.LOCKING_DATA.getValue())) {
-                            localStatus = Status.LOCKED_DATA.getValue();
-                        } else if (status.equals(Status.REGISTERING.getValue())) {
-                            localStatus = Status.MINTED.getValue();
-                        }
-                        // delete jobURL property
-                        doiContainerNode.getProperties().remove(new NodeProperty(DOI.VOSPACE_DOI_JOB_URL_PROPERTY));
-                        doiContainerNode.getProperty(DOI.VOSPACE_DOI_STATUS_PROPERTY).setValue(localStatus);
-                        vospaceDoiClient.getVOSpaceClient().setNode(vosuri, doiContainerNode);
-                        break;
-                    case ERROR:
-                    case ABORTED:
-                    case UNKNOWN:
-                    case SUSPENDED:
-                    case HELD:
-                        // assume job resulted in error, set corresponding status
-                        if (status.equals(Status.LOCKING_DATA.getValue())) {
-                            localStatus = Status.ERROR_LOCKING_DATA.getValue();
-                        } else if (status.equals(Status.REGISTERING.getValue())) {
-                            localStatus = Status.ERROR_REGISTERING.getValue();
-                        }
-                        // delete jobURL property
-                        doiContainerNode.getProperties().remove(new NodeProperty(DOI.VOSPACE_DOI_JOB_URL_PROPERTY));
-                        doiContainerNode.getProperty(DOI.VOSPACE_DOI_STATUS_PROPERTY).setValue(localStatus);
-                        vospaceDoiClient.getVOSpaceClient().setNode(vosuri, doiContainerNode);
-                        break;
-                    case PENDING:
-                    case QUEUED:
-                    case EXECUTING:
-                        // job is in progress, do nothing
-                        break;
-                    default:
-                        // do nothing
+
+                ExecutionPhase phase;
+                try {
+                    phase = recursiveSetNode.getPhase(20); // seconds
+                } catch (RuntimeException ex) {
+                    log.error("error getting recursive lock nodes job status for " + doiContainerNode.getName(), ex);
+                    phase = ExecutionPhase.ERROR;
+                }
+
+                if (phase != null) {
+                    switch (phase) {
+                        case COMPLETED:
+                        case ARCHIVED:
+                            // job finished, set corresponding status
+                            if (status.equals(Status.LOCKING_DATA.getValue())) {
+                                localStatus = Status.LOCKED_DATA.getValue();
+                            } else if (status.equals(Status.REGISTERING.getValue())) {
+                                localStatus = Status.MINTED.getValue();
+                            }
+                            // delete jobURL property
+                            doiContainerNode.getProperties().remove(new NodeProperty(DOI.VOSPACE_DOI_JOB_URL_PROPERTY));
+                            doiContainerNode.getProperty(DOI.VOSPACE_DOI_STATUS_PROPERTY).setValue(localStatus);
+                            vospaceDoiClient.getVOSpaceClient().setNode(vosuri, doiContainerNode);
+                            break;
+                        case ERROR:
+                        case ABORTED:
+                        case UNKNOWN:
+                        case SUSPENDED:
+                        case HELD:
+                            // assume job resulted in error, set corresponding status
+                            if (status.equals(Status.LOCKING_DATA.getValue())) {
+                                localStatus = Status.ERROR_LOCKING_DATA.getValue();
+                            } else if (status.equals(Status.REGISTERING.getValue())) {
+                                localStatus = Status.ERROR_REGISTERING.getValue();
+                            }
+                            // delete jobURL property
+                            doiContainerNode.getProperties().remove(new NodeProperty(DOI.VOSPACE_DOI_JOB_URL_PROPERTY));
+                            doiContainerNode.getProperty(DOI.VOSPACE_DOI_STATUS_PROPERTY).setValue(localStatus);
+                            vospaceDoiClient.getVOSpaceClient().setNode(vosuri, doiContainerNode);
+                            break;
+                        case PENDING:
+                        case QUEUED:
+                        case EXECUTING:
+                            // job is in progress, do nothing
+                            break;
+                        default:
+                            // do nothing
+                    }
                 }
             }
             return localStatus;
